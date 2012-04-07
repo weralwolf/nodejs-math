ab254cac5c386f88942442f2c9c35f362f868086
Anatolii Koval <weralwolf@gmail.com>
2012-04-07 13:33:24 +0300

    Vector operators implementation

--- Vector.js
+++ Vector.js
@@ -1,35 +1,114 @@
-var inh = require('./inheritance.js');
+/**
+ * I'd like to make some convention about
+ */
 
 var Vector = function() {
-	this.data = [];
-	for (i in arguments) {
-		this.data.push(arguments[i]);
-	}
-	;
-
-	this.toString = function() {
-		var str = '';
-		for ( var i = 0; i < this.data.length; i++) {
-			str += this.data[i].toString() + ' ';
-		}
-		;
-		return str;
-	};
-
-	this.at = function(i) {
-		return this.data[i];
-	};
-
-	this.set = function(i, val) {
-		this.data[i] = val;
-		return this;
-	};
-
-	this.dim = function() {
-		return this.data.length;
-	};
-	
-	this.mult = function(vector) {
-	};
+	// The Vector object is actually just the init constructor 'enhanced'
+	return new Vector.fn.init(arguments);
 };
 
+Vector.fn = Vector.prototype = {
+	// ---------------------- FIELDS -----------------------
+	type : "Vector",
+	operators : {
+		add : function(a, b) {
+			return a + b;
+		},
+		div : function(a, b) {
+			return a / b;
+		},
+		sub : function(a, b) {
+			return a - b;
+		},
+		mult : function(a, b) {
+			return a * b;
+		}
+	},
+	exceptions : {
+		operatorArgumentType : function(operator, arg) {
+			throw "Operator `"
+					+ operator.toString()
+					+ "` argument type violation.\n"
+					+ "Operation couldn't be done with such type of argument.\nArgument: "
+					+ arg.toString();
+		},
+		operatorArgumentDim : function(operator, arg) {
+			throw "Operator `" + operator.toString()
+					+ "` argument dim violation.\n"
+					+ "Arguments dimensions should be equals.\nArgument: "
+					+ arg.toString();
+		},
+		operatorNotImplemented : function(operator, arg) {
+			throw "Operator `" + operator.toString()
+					+ "` does not implemented.\n" + "There is no `"
+					+ arg.toString() + "` operator.";
+		},
+	},
+
+	// ---------------------- METHODS ----------------------
+	constructor : Vector,
+	init : function() {
+		this.data = [];
+		var args = arguments['0'];
+		for (i in args) {
+			this.data.push(args[i]);
+		}
+		;
+	},
+	push : function(val) {
+		this.data.push(val);
+	},
+	pop : function() {
+		return this.data.pop();
+	},
+	size : function() {
+		return this.data.length;
+	},
+	toString : function() {
+		return this.data.toString();
+	},
+	add : function(val) {
+		return this._byElementOperator("add", val);
+	},
+	sub : function(val) {
+		return this._byElementOperator("sub", val);
+	},
+	div : function(val) {
+		return this._byElementOperator("div", val);
+	},
+	mult : function(val) {
+		return this._byElementOperator("mult", val);
+	},
+
+	_byElementOperator : function(operatorType, val) {
+		if (this.operators[operatorType] === undefined) {
+			this.exceptions.operatorNotImplemented("Vector._byElementOperator",
+					val);
+		}
+		;
+		var result = new Vector();
+		var operatorName = "Vector." + operatorType.toString();
+		if (val instanceof Vector) {
+			if (val.size() == this.size()) {
+				for ( var i = 0; i < this.size(); i++) {
+					result.push(this.operators[operatorType](this.data[i],
+							val.data[i]));
+				}
+				;
+			} else {
+				this.exceptions.operatorArgumentDim(operatorName, val);
+			}
+			;
+		} else {
+			this.exceptions.operatorArgumentType(operatorName, val);
+		}
+		;
+		return result;
+	},
+};
+
+Vector.fn.init.prototype = Vector.fn;
+
+var f = new Vector(1, 2, 3);
+var g = new Vector(5, 6, 7);
+console.log(f.mult(g).toString());
